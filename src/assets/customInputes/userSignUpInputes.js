1bb41f0135da5479bdfaa057f2428f23ed4a596e
kabindrr <chaina_kabindra@yahoo.com>
2024-12-25 23:04:23 +1100

    custom inputes

---
+++ src/assets/customInputes/userSignUpInputes.js
@@ -0,0 +1,44 @@
+export const signUpInputes = [
+  {
+    label: "First Name *",
+    name: "fName",
+    type: "text",
+    required: true,
+    placeholder: "John",
+  },
+  {
+    label: "Last Name *",
+    name: "lName",
+    type: "text",
+    required: true,
+    placeholder: "Doe",
+  },
+  {
+    label: "Email *",
+    name: "email",
+    type: "text",
+    required: true,
+    placeholder: "john.doe@email.com",
+  },
+  {
+    label: "Phone Number",
+    name: "phone",
+    type: "number",
+
+    placeholder: "123456789",
+  },
+  {
+    label: "Password *",
+    name: "password",
+    type: "password",
+    required: true,
+    placeholder: "xxxxxxxx",
+  },
+  {
+    label: "Confirm Password *",
+    name: "confirmPassword",
+    type: "password",
+    required: true,
+    placeholder: "xxxxxxxx",
+  },
+];
--- src/pages/auth/SignUpPage.jsx
+++ src/pages/auth/SignUpPage.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { Button, Form } from "react-bootstrap";
+import { CustomInput } from "../../components/customInpute/CustomInput";
+import { signUpInputes } from "../../assets/customInputes/userSignUpInputes";
 
 export const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  return (
+    <div className="d-flex justify-content-center">
+      <Form style={{ width: "450px" }} className="card p-3 mt-5 shadow-lg mb-5">
+        <h3>Join our expanding community today</h3>
+        {signUpInputes.map((inpute) => (
+          <CustomInput key={inpute.name} {...inpute} />
+        ))}
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 };
