1312cb319948802d124a4ce662d576a892d2b718
kabindrr <chaina_kabindra@yahoo.com>
2024-12-26 23:51:49 +1100

    handleon change

--- src/pages/auth/SignUpPage.jsx
+++ src/pages/auth/SignUpPage.jsx
@@ -4,6 +4,12 @@ import { CustomInput } from "../../components/customInpute/CustomInput";
 import { signUpInputes } from "../../assets/customInputes/userSignUpInputes";
 
 export const SignUpPage = () => {
+  const handleOnChange = () => {};
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="d-flex justify-content-center">
       <Form style={{ width: "450px" }} className="card p-3 mt-5 shadow-lg mb-5">
