1994e0c8537c41b33add901d7095b9e803340a42
kabindrr <chaina_kabindra@yahoo.com>
2024-12-27 13:52:05 +1100

    custom hooks

--- src/pages/auth/SignUpPage.jsx
+++ src/pages/auth/SignUpPage.jsx
@@ -2,20 +2,32 @@ import React from "react";
 import { Button, Form } from "react-bootstrap";
 import { CustomInput } from "../../components/customInpute/CustomInput";
 import { signUpInputes } from "../../assets/customInputes/userSignUpInputes";
+import useForm from "../../hooks/useForm";
 
+const initialState = {};
 export const SignUpPage = () => {
-  const handleOnChange = () => {};
+  const { form, setForm, handleOnChange } = useForm(initialState);
+  console.log(form);
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
+    console.log(form);
   };
 
   return (
     <div className="d-flex justify-content-center">
-      <Form style={{ width: "450px" }} className="card p-3 mt-5 shadow-lg mb-5">
+      <Form
+        style={{ width: "450px" }}
+        className="card p-3 mt-5 shadow-lg mb-5"
+        onSubmit={handleOnSubmit}
+      >
         <h3>Join our expanding community today</h3>
         {signUpInputes.map((inpute) => (
-          <CustomInput key={inpute.name} {...inpute} />
+          <CustomInput
+            key={inpute.name}
+            {...inpute}
+            onChange={handleOnChange}
+          />
         ))}
 
         <Button variant="primary" type="submit">
