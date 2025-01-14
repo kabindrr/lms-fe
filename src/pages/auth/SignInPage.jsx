27dbf8f17724a5bb1e9ad291b77ac4d022e537c9
kabindrr <chaina_kabindra@yahoo.com>
2025-01-14 12:57:51 +1100

    login page ui

--- src/pages/auth/SignInPage.jsx
+++ src/pages/auth/SignInPage.jsx
@@ -1,5 +1,54 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import { Card, Form } from "react-bootstrap";
+import { CustomInput } from "../../components/customInpute/CustomInput";
+import useForm from "../../hooks/useForm";
+
+const initialState = {};
 
 export const SignInPage = () => {
-  return <div>SignInPage</div>;
+  const { form, setForm, handleOnChange, passwordErrors } =
+    useForm(initialState);
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+
+  return (
+    <div className="SignInPage d-flex justify-content-center align-items-center">
+      {" "}
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Title>Welcome to Our Library</Card.Title>
+          <hr />
+          <Form onSubmit={handleOnSubmit}>
+            <CustomInput
+              label="Email"
+              name="email"
+              type="email"
+              required
+              placeholder="name@gmail.com"
+              onChange={handleOnChange}
+            />
+            <CustomInput
+              label="Password"
+              name="password"
+              type="password"
+              required
+              placeholder="********"
+              onChange={handleOnChange}
+            />
+            <div className="d-grid">
+              <Button type="submit">Sign In</Button>
+            </div>
+          </Form>
+          <div className="text-end my-3 ">
+            Forgot Password?
+            <a href="/forgot-password">Reset Now!</a>{" "}
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
