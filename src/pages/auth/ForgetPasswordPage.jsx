5d177b336c4da96b2486ffa693a344a4eeabb8cd
kabindrr <chaina_kabindra@yahoo.com>
2025-01-31 00:26:47 +1100

    otp

--- src/pages/auth/ForgetPasswordPage.jsx
+++ src/pages/auth/ForgetPasswordPage.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { Button, Card, Form } from "react-bootstrap";
+import { Alert, Button, Card, Form } from "react-bootstrap";
 import { CustomInput } from "../../components/customInpute/CustomInput";
-
+import useForm from "../../hooks/useForm";
+const initialState = {};
 export const ForgetPasswordPage = () => {
+  const { form, setForm, handleOnChange, passwordErrors } =
+    useForm(initialState);
+
   const handleOnSubmit = (e) => {
     e.preventDefault();
+    console.log(form);
   };
 
   return (
@@ -21,12 +26,53 @@ export const ForgetPasswordPage = () => {
               type="email"
               required
               placeholder="name@gmail.com"
+              onChange={handleOnChange}
             />
 
             <div className="d-grid">
-              <Button type="submit">Sign In</Button>
+              <Button type="submit">Request OTP Now</Button>
             </div>
           </Form>
+          <hr />
+
+          {/* show this form once otp is requested */}
+          <div>
+            <Alert variant="success">
+              We will send you an OTP to your email, if email if found in our
+              system. Please check your junk/spam folder if you don't see email
+              in your Inbox
+            </Alert>
+            <Form onSubmit={handleOnSubmit}>
+              <CustomInput
+                label="OTP"
+                name="otp"
+                type="number"
+                required
+                placeholder="1234"
+                onChange={handleOnChange}
+              />
+              <CustomInput
+                label="New Password"
+                name="password"
+                type="password"
+                required
+                placeholder="**********"
+                onChange={handleOnChange}
+              />
+              <CustomInput
+                label="Confirm Password"
+                name="confirmPassword"
+                type="password"
+                required
+                placeholder="**********"
+                onChange={handleOnChange}
+              />
+
+              <div className="d-grid">
+                <Button type="submit">Request Password Now</Button>
+              </div>
+            </Form>
+          </div>
           <div className="text-end my-3 ">
             Ready to Login?<a href="/signin"> Login Now!</a>{" "}
           </div>
