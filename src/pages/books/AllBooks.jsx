99efaf1b2401aea6ba7b6a52b2e4b02595f44142
kabindrr <chaina_kabindra@yahoo.com>
2025-06-24 17:04:23 +1000

    pagination

--- src/pages/books/AllBooks.jsx
+++ src/pages/books/AllBooks.jsx
@@ -14,9 +14,33 @@ import {
 } from "../../components/customCards/CustomCard";
 import { Pagination } from "react-bootstrap";
 import { useSelector } from "react-redux";
+import { current } from "@reduxjs/toolkit";
+
+const booksPerScreen = 5;
 const AllBooks = () => {
   const [view, setView] = useState("card");
+  const [active, setActive] = useState(1);
   const { publicBooks } = useSelector((state) => state.bookInfo);
+
+  const pages = Math.ceil(publicBooks.length / booksPerScreen);
+
+  // GEt book for the current page
+  const startIndex = (active - 1) * booksPerScreen;
+  const endIndex = startIndex + booksPerScreen;
+  const displayBooks = publicBooks.slice(startIndex, endIndex);
+
+  let items = [];
+  for (let number = 1; number <= pages; number++) {
+    items.push(
+      <Pagination.Item
+        key={number}
+        active={number === active}
+        onClick={() => setActive(number)}
+      >
+        {number}
+      </Pagination.Item>
+    );
+  }
   return (
     <Container>
       <Row className="mt-3">
@@ -50,8 +74,8 @@ const AllBooks = () => {
           <div></div>
           <hr />
           <div className="bookList-Container d-flex gap-3 flex-wrap justify-content-center m-3">
-            {publicBooks.length > 0 &&
-              publicBooks.map((book) =>
+            {displayBooks.length > 0 &&
+              displayBooks.map((book) =>
                 view === "card" ? (
                   <CustomCard key={book._id} {...book} />
                 ) : (
@@ -59,7 +83,9 @@ const AllBooks = () => {
                 )
               )}
           </div>
-          <div className="pagination">todo pagination</div>
+          <div className="mt-3 d-flex justify-content-center">
+            <Pagination>{items}</Pagination>
+          </div>
         </Col>
       </Row>
     </Container>
